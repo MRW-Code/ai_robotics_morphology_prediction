d648f6f7cdbfd886c1ed3716d4b6b9d7f3ad6265
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-04-01 14:42:36 +0100

    Image Generating

--- csd_main.py
+++ csd_main.py
@@ -1,8 +1,9 @@
 from src.utils import args
 from src.factory import represent_solvents, generate_csd_output, get_api_representations
+from src.image_generator import ImageGenerator
 import pandas as pd
 from src.models import random_forest_classifier
-
+import os
 
 
 
@@ -11,7 +12,9 @@ if __name__ == '__main__':
     csd_df = generate_csd_output(min_sol_counts=100, min_habit_counts=1000, save_outputs=True)
 
     if args.input == 'image':
-        NotImplementedError()
+        if args.from_scratch or len(os.listdir('./checkpoints/inputs/images')) == 0:
+            gen = ImageGenerator(csd_df)
+
     else:
         api_features_df = get_api_representations(csd_df, save_outputs=True)
         labels = api_features_df['label']
@@ -21,9 +24,8 @@ if __name__ == '__main__':
     print('done')
 
 
-    # Make sure all other inputs work
-    ## add image inputs
+    # Make sure all other no image inputs work
+    ## add image models
     ### add models
-    #### add arguemtns to reduce the model complexity e.g single solvents.
-    ##### add solvent descriptors
+    #### Single solvent added maybe add single habits?
 
--- src/image_generator.py
+++ src/image_generator.py
@@ -4,36 +4,45 @@ from rdkit.Chem import Draw
 import tqdm
 from joblib import Parallel, delayed
 import os
+import numpy as np
+import PIL
 
 class ImageGenerator():
 
     def __init__(self, csd_output):
         print('Generating API Images')
-        self.dataset = csd_output
+        self.dataset = self.prep_df(csd_output)
+        self.api_smiles = self.dataset['api_smiles']
+        self.sol_smiles = self.dataset['sol_smiles']
+        self.refcodes = self.dataset['REFCODE']
+        self.sol_names = self.dataset['Solvent']
+        self.labels = self.dataset['Habit']
+        self.gen_all_images()
 
-        self.smiles = csd_output
-        self.smilesB = pd.read_csv('./data/cocrystal/component2_smiles.csv').smiles
-        self.idA = pd.read_csv('./data/cocrystal/component1_smiles.csv').api
-        self.idB = pd.read_csv('./data/cocrystal/component2_smiles.csv').api
-        self.gen_all_cc_images()
+    def prep_df(self, csd_output):
+        df = csd_output.rename(columns={'SMILES' : 'api_smiles'})
+        sol_df = pd.read_csv('./csd/raw_data/solvent_smiles.csv')
+        join = pd.merge(sol_df, df, on='Solvent').rename(columns={'SMILES': 'sol_smiles'})
+        return join
 
-    def smile_to_image(self, smile, compound_id):
-        mol = Chem.MolFromSmiles(smile)
-        draw = Draw.MolToFile(mol, (f'./data/{self.dataset}/images/{compound_id}.png'), size=[250, 250])
+    def smile_to_image(self, api_smile, refcode, sol_smile, sol_name, label):
+        try:
+            api_mol = Chem.MolFromSmiles(api_smile)
+            api_img = np.array(Draw.MolToImage(api_mol, size=[250, 250]))
+            sol_mol = Chem.MolFromSmiles(sol_smile)
+            sol_img = np.array(Draw.MolToImage(sol_mol, size=[250, 250]))
+            stacked = PIL.Image.fromarray(np.vstack([api_img, sol_img]))
+            stacked.save(f'./checkpoints/inputs/images/{refcode}_{sol_name}_{label}.png')
+        except:
+            print(f'{api_smile}, {refcode}, {sol_smile}, {sol_name}, {label}')
+        return None
 
-    def gen_all_solubility_images(self):
-        smiles = self.smiles
-        id = self.id
-        # [self.smile_to_image(s, i) for s, i in tqdm.tqdm(zip(smiles, id))]
-        Parallel(n_jobs=os.cpu_count())(delayed(self.smile_to_image)(s, i) for s, i in tqdm.tqdm(zip(smiles, id), ncols=80))
+    def gen_all_images(self):
+        os.makedirs('./checkpoints/inputs/images', exist_ok=True)
+        Parallel(n_jobs=os.cpu_count())\
+            (delayed(self.smile_to_image)(i, j, k, l, m) for i, j, k, l, m in tqdm.tqdm(zip(self.api_smiles,
+                                                                          self.refcodes,
+                                                                          self.sol_smiles,
+                                                                          self.sol_names,
+                                                                          self.labels), ncols=80))
 
-    def gen_all_cc_images(self):
-        smiles = self.smilesA
-        id = self.idA
-        # [self.smile_to_image(s, i) for s,i in tqdm.tqdm(zip(smiles, id))]
-        Parallel(n_jobs=os.cpu_count())(delayed(self.smile_to_image)(s, i) for s, i in tqdm.tqdm(zip(smiles, id), ncols=80))
-
-        smiles = self.smilesB
-        id = self.idB
-        # [self.smile_to_image(s, i) for s, i in tqdm.tqdm(zip(smiles, id))]
-        Parallel(n_jobs=os.cpu_count())(delayed(self.smile_to_image)(s, i) for s, i in tqdm.tqdm(zip(smiles, id), ncols=80))
