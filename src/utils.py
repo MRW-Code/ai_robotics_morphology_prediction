08e6e81904ad7defedc0b56543b0f2b742ce28df
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-04-01 15:29:23 +0100

    add optional gpu idx

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 parser = argparse.ArgumentParser(usage='python main.py')
 parser.add_argument('--from_scratch', action='store_true', dest='from_scratch')
@@ -11,16 +12,7 @@ parser.add_argument('-s', '--solvent', action='store', dest='solvent',
 parser.add_argument('-j', '--join_mode', action='store', dest='mode',
                     default='concat', choices=['concat', 'one_hot', 'drop'])
 parser.add_argument('--no_augs', action='store_true', dest='no_augs', default=False)
-
-# parser.add_argument('-m', '--model', action='store', dest='model',
-#                     default='RF', choices=['RF', 'ResNet'])
-# parser.add_argument('--kfold', action='store_true', dest='kfold')
-# parser.add_argument('-u', '--user', action='store', dest='user',
-#                     default='matt', choices=['laura', 'matt'])
-# parser.add_argument('--gen_data', action='store_false', dest='load_data')
-# parser.add_argument('--deep', action='store_true', dest='is_deep')
-# parser.add_argument('--dset', action='store', dest='dset',
-#                     default='descriptor', choices=['descriptor', 'image'])
-# parser.add_argument('--binary', action='store_false', dest='binary')
-
+parser.add_argument('--gpu_idx', action='store', dest='gpu_idx', default='0',
+                  choices=['0', '1', '2', '3', '4', '5'])
 args = parser.parse_args()
+os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_idx
