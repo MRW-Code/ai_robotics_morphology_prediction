0197a27ba3f4641cd271e51fb112d46e0b365417
Matthew Wilkinson <mattwilkinson16@hotmail.co.uk>
2022-04-27 12:08:39 +0100

    minor fix

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,7 @@ parser.add_argument('-i', '--input', action='store', dest='input',
                                               'mol2vec', 'ecfp', 'pubchem_fp', 'maccs',
                                               'spectrophore', 'weave_graph'])
 parser.add_argument('-s', '--solvent', action='store', dest='solvent',
-                    default='all', choices=['all', 'ethanol', 'methanol', 'raw_images',
+                    default='all', choices=['all', 'ethanol', 'methanol', 'water',
                                             'ethyl acetate', 'acetone',
                                             'hexane', 'acetonitrile',
                                             'diethyl ether', 'toluene',
