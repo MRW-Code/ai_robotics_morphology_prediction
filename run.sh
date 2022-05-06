06d9096ceeb41ca405ba0ee116bcd4d99f0e811f
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-05-06 09:45:38 +0100

    new main

--- run.sh
+++ run.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-python3 csd_main.py -j drop --solvent "heptane" -i image
+python3 csd_main.py -j drop --solvent "ethanol" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "methanol" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "ethyl acetate" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "acetone" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "hexane" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "acetonitrile" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "diethyl ether" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "toluene" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "benzene" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "pentane" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "tetrahydrofuran" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "water" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "dimethylsulfoxide" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "isopropanol" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "dimethylformamide" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "cyclohexane" -i mordred_descriptor
+python3 csd_main.py -j drop --solvent "heptane" -i mordred_descriptor
+
 
 
 
