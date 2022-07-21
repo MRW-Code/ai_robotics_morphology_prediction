9ffda1d4eb7f9ad13acd52fd55f1a487a20590ae
Matthew Wilkinson <mattwilkinson16@hotmail.co.uk>
2022-07-21 22:45:25 +0100

    external testing

--- dl_morph_labelling/model.py
+++ dl_morph_labelling/model.py
@@ -43,15 +43,15 @@ def robot_train_fastai_model_classification(model_df, count):
                                    item_tfms=None,
                                    batch_tfms=None,
                                    y_block=CategoryBlock(),
-                                   bs=8,
+                                   bs=32,
                                    shuffle=True)
     metrics = [error_rate, accuracy]
-    learn = cnn_learner(dls, resnet18, metrics=metrics)
-    learn.fine_tune(10, cbs=[SaveModelCallback(monitor='accuracy', fname=f'./{args.no_augs}_best_cbs.pth'),
+    learn = vision_learner(dls, args.model, metrics=metrics)
+    learn.fine_tune(20, cbs=[SaveModelCallback(monitor='valid_loss', fname=f'./{args.no_augs}_best_cbs.pth'),
                             ReduceLROnPlateau(monitor='valid_loss',
                                               min_delta=0.1,
                                               patience=3),
-                             EarlyStoppingCallback(monitor='accuracy', min_delta=0.1, patience=5)])
+                             EarlyStoppingCallback(monitor='accuracy', min_delta=0.1, patience=10)])
 
     os.makedirs(f'./dl_morph_labelling/checkpoints/figures/{args.model}', exist_ok=True)
     interp = ClassificationInterpretation.from_learner(learn)
@@ -61,30 +61,6 @@ def robot_train_fastai_model_classification(model_df, count):
     print(learn.validate())
     learn.export(f'./dl_morph_labelling/checkpoints/models/{args.model}/trained_model_{args.no_augs}_{count}.pkl')
 
-def robot_external_test_model(model_df):
-    dls = ImageDataLoaders.from_df(model_df,
-                                   fn_col=0,
-                                   label_col=1,
-                                   valid_col=2,
-                                   item_tfms=None,
-                                   batch_tfms=None,
-                                   y_block=CategoryBlock(),
-                                   bs=8,
-                                   shuffle=True)
-    metrics = [error_rate, accuracy]
-    learn = cnn_learner(dls, resnet18, metrics=metrics)
-    learn.fine_tune(5)
-
-    os.makedirs('./dl_morph_labelling/checkpoints/test/figures', exist_ok=True)
-    interp = ClassificationInterpretation.from_learner(learn)
-    interp.plot_confusion_matrix()
-    plt.savefig(f'./dl_morph_labelling/checkpoints/test/figures/conf_mtrx_external_test')
-
-    print(learn.validate())
-    # learn.export(f'./dl_morph_labelling/checkpoints/models/trained_model_{args.no_augs}_{count}.pkl')
-
-
-
 def robot_kfold_fastai(robot_df, n_splits):
     print(f'Training Robot FastAI model with no_augs = {args.no_augs}')
     os.makedirs(f'./dl_morph_labelling/checkpoints/models/{args.model}/', exist_ok=True)
@@ -93,6 +69,7 @@ def robot_kfold_fastai(robot_df, n_splits):
     kfold = StratifiedKFold(n_splits=n_splits, shuffle=True)
     count = 0
     best_metrics = []
+    test_metrics = []
     for train_index, val_index in tqdm(kfold.split(paths, labels)):
         X_train, X_val = paths[train_index], paths[val_index]
         y_train, y_val = labels[train_index], labels[val_index]
@@ -114,29 +91,21 @@ def robot_kfold_fastai(robot_df, n_splits):
         trainer = robot_train_fastai_model_classification(model_df, count)
         model = load_learner(f'./dl_morph_labelling/checkpoints/models/{args.model}/trained_model_{args.no_augs}_{count}.pkl', cpu=False)
         best_metrics.append(model.final_record)
+
+        if args.robot_test:
+            path = './dl_morph_labelling/external_test_robot'
+
+            model = load_learner(
+                f'./dl_morph_labelling/checkpoints/models/{args.model}/trained_model_{args.no_augs}_{count}.pkl',
+                cpu=False)
+            test_dl = model.dls.test_dl(get_robot_external_set(), with_labels=True)
+            preds, _, decoded = model.get_preds(dl=test_dl, with_decoded=True)
+            print(accuracy_score(_, decoded))
+            test_metrics.append(accuracy_score(_, decoded))
+
         count += 1
 
     print(best_metrics)
-    print(f'mean acc = {np.mean([best_metrics[x][2] for x in range(n_splits)])}')
+    print(f'mean valid acc = {np.mean([best_metrics[x][2] for x in range(n_splits)])}')
+    print(f'mean test acc = {np.mean(test_metrics)}')
     return None
-
-def robot_external_test(robot_df):
-    print(f'Training Robot for External Testing')
-    paths = robot_df.fname
-    labels = robot_df.label
-    train_df = pd.DataFrame({'fname': paths, 'label': labels})
-    train_df.loc[:, 'is_valid'] = 0
-    test_df = get_robot_external_set()
-    test_df.loc[:, 'is_valid'] = 1
-
-    if args.no_augs:
-        model_df = pd.concat([train_df, test_df])
-    else:
-        raw_model_df = pd.concat([train_df, test_df])
-        augmentor = RobotImageAugmentations()
-        augmentor.do_image_augmentations(raw_model_df)
-        aug_model_df = get_robot_aug_df()
-        model_df = pd.concat([aug_model_df, test_df])
-    print('done')
-
-    trainer = robot_external_test_model(model_df)
