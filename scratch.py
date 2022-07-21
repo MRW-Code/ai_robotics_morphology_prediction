f225219d48bf9b842a70aa63afa2730258583db2
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-07-21 14:58:26 +0100

    scratch plot making

--- scratch.py
+++ scratch.py
@@ -1,11 +1,25 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# arry1 = np.array([0.4242105263157895,
+#                   0.40315789473684205,
+#                   0.4342105263157895,
+#                   0.36210526315789476,
+#                   0.4131578947368421])
+#
+# mean = np.mean(arry1)
+# print(mean)
 
+# df = pd.read_csv('./checkpoints/inputs/mordred_descriptor_dataset.csv')
+df = pd.read_csv('./csd/processed/raw_csd_output.csv')
 
-arry1 = np.array([0.4242105263157895,
-                  0.40315789473684205,
-                  0.4342105263157895,
-                  0.36210526315789476,
-                  0.4131578947368421])
+x = df.Habit.value_counts()[0:30]
 
-mean = np.mean(arry1)
-print(mean)
+plt.bar(x.index, x.values)
+plt.xlabel('CSD Morphology Label')
+plt.ylabel('Number of Data Points')
+plt.xticks(rotation=90)
+plt.savefig('./test', bbox_inches='tight')
+# plt.show()
+
+print('done')
