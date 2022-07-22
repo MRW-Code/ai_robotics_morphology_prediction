83e2cfbba6a37d0201479d6a8b6d539237061d46
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-07-22 15:37:46 +0100

    requirements.txt

--- dl_morph_labelling/model.py
+++ dl_morph_labelling/model.py
@@ -41,7 +41,7 @@ def robot_train_fastai_model_classification(model_df, count):
                                    label_col=1,
                                    valid_col=2,
                                    item_tfms=None,
-                                   batch_tfms=Resize(255,255),
+                                   batch_tfms=Resize(224,224),
                                    y_block=CategoryBlock(),
                                    bs=8,
                                    shuffle=True)
