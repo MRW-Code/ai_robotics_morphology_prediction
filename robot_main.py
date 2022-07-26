40217e3e694e094ed05523a0bf63e379ea45b692
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-07-26 14:25:20 +0100

    models

--- robot_main.py
+++ robot_main.py
@@ -1,9 +1,11 @@
+from src.utils import args
 from dl_morph_labelling.preprocessing import get_robot_labelling_df
 from dl_morph_labelling.model import robot_kfold_fastai
 import pandas as pd
 import os
 
 if __name__ == '__main__':
+    print(args.gpu_idx)
     os.makedirs('./dl_morph_labelling/checkpoints', exist_ok=True)
     robot_df = get_robot_labelling_df()
     robot_kfold_fastai(robot_df, n_splits=5)
